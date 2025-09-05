5de3c3b98b281ecc3323438335d5d2f45fbf439e
davishek7 <davishek7@gmail.com>
2025-09-05 14:27:41 +0530

    fixed api url in work loader

--- src/loaders/works.loader.js
+++ src/loaders/works.loader.js
@@ -1,5 +1,5 @@
 export async function worksLoader(){
-    const response = await fetch("http://127.0.0.1:8000/api/gallery/?category=gallery")
+    const response = await fetch("gallery/?category=gallery")
     const responseData = await response.json()
     const data = await responseData.data
     return data
