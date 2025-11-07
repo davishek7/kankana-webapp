26635c6a5cb7d45bbc3ee395de960b831e635c57
davishek7 <davishek7@gmail.com>
2025-11-07 22:44:00 +0530

    changed dashboard design

--- src/components/admin/DashboardCard.jsx
+++ src/components/admin/DashboardCard.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-function DashboardCard({ title, count, amount, textClass}) {
+function DashboardCard({ title, count, amount, bgClass }) {
   return (
     <div className="col-md-4 col-lg-2">
-      <div className="card text-center shadow-sm border-0">
+      <div className={`card ${bgClass} text-center shadow-sm border-0`}>
         <div className="card-body">
-          <h6 className="card-title text-muted">{title}</h6>
-          <h3 className={`fw-bold ${textClass}`}>{count ?? amount}</h3>
+          <h6 className="card-title">{title}</h6>
+          <h3 className="fw-bold">{count ?? amount}</h3>
         </div>
       </div>
     </div>
--- src/routes/admin/Dashboard.jsx
+++ src/routes/admin/Dashboard.jsx
@@ -7,28 +7,36 @@ export default function Dashboard() {
 
   return (
     <div className="container my-4">
+      <h1 className="text-center mb-3">Kankana&apos;s Makeover</h1>
       <div className="row g-3">
         {/* Total Images */}
-        <DashboardCard title="Total Images" count={data?.total_images} />
+        <DashboardCard title="Total Images" count={data?.total_images} bgClass="text-bg-warning" />
 
         {/* Contacts Received */}
-        <DashboardCard title="Contacts Received" count={data?.total_contacts} />
+        <DashboardCard title="Contacts Received" count={data?.total_contacts} bgClass="text-bg-light"/>
 
         {/* Total Bookings */}
-        <DashboardCard title="Total Bookings" count={data?.total_bookings} />
+        <DashboardCard title="Total Bookings" count={data?.total_bookings} bgClass="text-bg-dark"/>
 
         {/* Total Revenue */}
         <DashboardCard
-          title="Total Revenue"
+          title="Total Revenue (Received + Due)"
           amount={`₹${data?.total_revenue}`}
-          textClass="text-success"
+          bgClass="text-bg-success"
+        />
+
+        {/* Total Received */}
+        <DashboardCard
+          title="Total Received"
+          amount={`₹${data?.total_received}`}
+          bgClass="text-bg-info"
         />
 
         {/* Total Due */}
         <DashboardCard
           title="Total Due"
           amount={`₹${data?.total_due}`}
-          textClass="text-danger"
+          bgClass="text-bg-danger"
         />
       </div>
     </div>
